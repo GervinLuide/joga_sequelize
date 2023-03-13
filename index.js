73470f0ec267517975c12f6696078acf981dcd71
Gervin Luide <gervin.luide@khk.ee>
2023-03-13 16:42:57 +0200

    Created connection to database

--- index.js
+++ index.js
@@ -2,9 +2,24 @@ const express = require("express")
 const app = express()
 
 //parse request of content type aplication/json
-app.use(express.json())
+app.use(express.json());
 //parse request of content type application/x-ww-form-urlencoded
 app.use(express.urlencoded({extended:true}));
+
+//connect to database
+const Sequelize = require("sequelize");
+const sequalize = new Sequelize('mysql://root:qwerty@localhost:3306/joga_sequelize')
+
+//testing connection
+sequalize
+    .authenticate()
+    .then(() => {
+        console.log('Connected to the database');
+    })
+    .catch(err =>{
+        console.error('Unable to connect to the database:', err);
+    });
+
 //simple route
 app.get ("/", (req,res) =>{
     res.json({ message: "Welcome to sequelize application."});
